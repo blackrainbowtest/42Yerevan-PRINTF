ae5ebfb927684f698cb9b894e7fa52a42aa9d5c1
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-02-23 15:53:55 +0400

    add

--- mian.c
+++ mian.c
@@ -20,7 +20,25 @@ int	ft_write_str(char *str)
 	return (count);
 }
 
+int	ft_write_dig(long n, int base)
+{
+	int		count;
+	chat	*symbols;
 
+	symbols = "0123456789abcdef";
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		return (ft_write_dig(-n, base) + 1);
+	}
+	else if (n < base)
+		return ft_write_chr(symbols[n]);
+	else
+	{
+		count = ft_write_dig(n / base, base);
+		return (count + ft_write_dig(n % base, base));
+	}
+}
 
 int	ft_print(char specifier, va_list ap)
 {
@@ -31,9 +49,9 @@ int	ft_print(char specifier, va_list ap)
 	else if (specifier == 's')
 		count += ft_write_str(va_arg(ap, char *));
 	else if (specifier == 'd')
-		count += ft_write_dig(va_arg(ap, int), 10);
+		count += ft_write_dig((long)va_arg(ap, int), 10);
 	else if (specifier == 'x')
-		count += ft_write_dig(va_arg(ap, unsigned int), 16);
+		count += ft_write_dig((long)va_arg(ap, unsigned int), 16);
 	else
 		count += write(1, &specifier, 1);
 	return (count);
@@ -62,9 +80,11 @@ int	main(void)
 {
 	int		count;
 
-	count = ft_printf("test %s test %d\n", "sss", 666);
+	// count = ft_printf("test %s test %d\n", "sss", 666);
 
-	ft_printf("The characters writen %d\n", count);
+	// ft_printf("The characters writen %d\n", count);
+	count = ft_printf("hex numbers %x", 42);
+	ft_printf("printed chars count: %d\n", count);
 
 	return (0);
 }
