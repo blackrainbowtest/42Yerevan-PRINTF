0d4493d9078805a5ad1e3f89b259e1bea72d4632
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-02-23 15:40:37 +0400

    add

--- mian.c
+++ mian.c
@@ -2,6 +2,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int	ft_write_chr(int c)
+{
+	return (write(1, &c, 1));
+}
+
+int	ft_write_str(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		count += ft_write_chr((int)*str);
+		++str;
+	}
+	return (count);
+}
+
+
+
 int	ft_print(char specifier, va_list ap)
 {
 	int		count;
@@ -12,7 +32,7 @@ int	ft_print(char specifier, va_list ap)
 		count += ft_write_str(va_arg(ap, char *));
 	else if (specifier == 'd')
 		count += ft_write_dig(va_arg(ap, int), 10);
-	else if (specifier == 'd')
+	else if (specifier == 'x')
 		count += ft_write_dig(va_arg(ap, unsigned int), 16);
 	else
 		count += write(1, &specifier, 1);
