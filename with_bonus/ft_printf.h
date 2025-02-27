471601c5345fe80cf3f82e79c9f3badb01e82547
= <=>
2025-02-27 21:29:21 +0400

    add

--- with_bonus/ft_printf.h
+++ with_bonus/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 16:42:58 by aramarak          #+#    #+#             */
-/*   Updated: 2025/02/23 16:43:52 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/02/27 19:44:43 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,15 @@
 # include <stdlib.h>
 # include <stdarg.h>
 # include <limits.h>
-# if defined(__linux__)
-#  define DEFAULTNULL "(nil)"
-# elif defined(__APPLE__)
-#  define DEFAULTNULL "0x0"
-# endif
 
-typedef	struct	s_keys
+typedef struct s_keys
 {
 	int	minus_left;
 	int	zero_space;
 	int	dot_precision;
 	int	width;
-}				t_keys
+}			t_keysl;
 
-// no bonus part
 int	ft_write_str(char *str);
 int	ft_write_dig(long n, int base, int capital);
 int	ft_write_dig_unsigned(unsigned long n, int base, int capital);
@@ -39,5 +33,11 @@ int	ft_write_chr(int c);
 int	ft_printf(const char	*format, ...);
 int	ft_handle_format(char specifier, va_list ap);
 int	ft_write_ptr(void *ptr);
+
+# if defined(__linux__)
+#  define DEFAULTNULL "(nil)"
+# elif defined(__APPLE__)
+#  define DEFAULTNULL "0x0"
+# endif
 
 #endif
