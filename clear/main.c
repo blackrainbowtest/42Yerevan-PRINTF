6d540d3de8f3fc9d2221fab5dc03c18bddfa3e48
= <=>
2025-03-02 15:01:30 +0400

    update testes INT_MIN

--- clear/main.c
+++ clear/main.c
@@ -55,5 +55,70 @@ int	main()
     printf("\nTest 12 (точность 0 и число 0):\n");
     printf("printf   : |%.0d|\n", n3);
     ft_printf("ft_printf: |%.0d|\n", n3);
+
+    printf("\nTest 13 (минимальное отрицвтельное число -2^32):\n");
+    printf("printf   : |%d|\n", INT_MIN);
+    ft_printf("ft_printf: |%d|\n", INT_MIN);
+
+
+/*
+    unsigned int u1 = 42;
+    unsigned int u2 = 4294967254; // Это 2^32 - 42 (максимальное 32-битное беззнаковое число - 42)
+    unsigned int u3 = 0;
+
+    printf("Test 1 (обычное число):\n");
+    printf("printf   : |%u|\n", u1);
+    ft_printf("ft_printf: |%u|\n", u1);
+
+    printf("\nTest 2 (большое число):\n");
+    printf("printf   : |%u|\n", u2);
+    ft_printf("ft_printf: |%u|\n", u2);
+
+    printf("\nTest 3 (ноль):\n");
+    printf("printf   : |%u|\n", u3);
+    ft_printf("ft_printf: |%u|\n", u3);
+
+    printf("\nTest 4 (ширина 5):\n");
+    printf("printf   : |%5u|\n", u1);
+    ft_printf("ft_printf: |%5u|\n", u1);
+
+    printf("\nTest 5 (ширина 10):\n");
+    printf("printf   : |%10u|\n", u2);
+    ft_printf("ft_printf: |%10u|\n", u2);
+
+    printf("\nTest 6 (флаг -):\n");
+    printf("printf   : |%-10u|\n", u1);
+    ft_printf("ft_printf: |%-10u|\n", u1);
+
+    printf("\nTest 7 (флаг 0):\n");
+    printf("printf   : |%010u|\n", u1);
+    ft_printf("ft_printf: |%010u|\n", u1);
+
+    printf("\nTest 8 (флаг 0 с большим числом):\n");
+    printf("printf   : |%010u|\n", u2);
+    ft_printf("ft_printf: |%010u|\n", u2);
+
+    printf("\nTest 9 (ширина 10, точность 5):\n");
+    printf("printf   : |%10.5u|\n", u1);
+    ft_printf("ft_printf: |%10.5u|\n", u1);
+
+    printf("\nTest 10 (ширина 10, точность 5, большое число):\n");
+    printf("printf   : |%10.5u|\n", u2);
+    ft_printf("ft_printf: |%10.5u|\n", u2);
+
+    printf("\nTest 11 (точность больше числа):\n");
+    printf("printf   : |%.5u|\n", u1);
+    ft_printf("ft_printf: |%.5u|\n", u1);
+
+    printf("\nTest 12 (точность 0 и число 0):\n");
+    printf("printf   : |%.0u|\n", u3);
+    ft_printf("ft_printf: |%.0u|\n", u3);
+
+    printf("\nTest 13 (наименьшее число инт беззнаковое):\n");
+    printf("printf   : |%u|\n", INT_MIN);
+    ft_printf("ft_printf: |%u|\n", INT_MIN);
+*/
+
+
 	return (0);
 }
